59b4be97f2c5fc0cb5d08c8287dfde1bfbaf1a33
iti <itish.02@gmail.com>
2021-05-09 21:18:55 +0530

    binary heap implementation

--- bst.js
+++ bst.js
@@ -115,3 +115,39 @@ tree.insert(8);
 tree.insert(20);
 tree.insert(17);
 tree.DFSPreOrder();
+
+class MaxBinaryHeap {
+    constructor() {
+        this.values = [];
+    }
+
+    insert(value) {
+        this.values.push(value)
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let index = this.values.length - 1;
+        const element = this.values[index];
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            let parent = this.values[parentIndex];
+            if (element <= parent) break;
+            this.values[parentIndex] = element;
+            this.values[index] = parent;
+            index = parentIndex;
+        }
+        return this;
+    };
+};
+
+let heap = new MaxBinaryHeap();
+heap.insert(41)
+heap.insert(39)
+heap.insert(33)
+heap.insert(18)
+heap.insert(27)
+heap.insert(12);
+heap.insert(55);
+heap.insert(1);
+heap.insert(45);
